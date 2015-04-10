a50194356fde4e45fc0782074cba9dd281a21caf
Kono <konosprod@free.fr>
2015-04-10 14:08:45 +0200

    First release

---
+++ PluginChoucroute/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPlugins
+{
+    public class PluginChoucroute : PluginInterface
+    {
+        public string Name
+        {
+            get
+            {
+                return "Choucroute";
+            }
+        }
+
+        public void Do()
+        {
+            System.Console.WriteLine("Choucroute !");
+        }
+    }
+}
---
+++ PluginHello/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPlugins
+{
+    public class PluginHello : PluginInterface
+    {
+        public string Name
+        {
+            get
+            {
+                return "Hello";
+            }
+        }
+
+        public void Do()
+        {
+            System.Console.WriteLine("Hello !");
+        }
+    }
+}
